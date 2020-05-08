0d52bcf9de3779cd19c0ef5acd88129e33325e6d
Andrew Parrish <andrew.e.parrish@gmail.com>
2020-05-08 23:25:46 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,15 +1,17 @@
 function callMe() {
+  var lyric = "maybe";
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
 
 function crazy() {
-  // fix the code in here:
+  var thisIsCrazy = function() {
+    console.log("hey!!!");
+  }
   thisIsCrazy();
 
   var thisIsCrazy = function (){
@@ -18,7 +20,9 @@ function crazy() {
 }
 
 function sayMyName() {
-  // fix the code in here:
+// enter your code here
+  }
+
   var name = "Cricky";
 
   sayMy();
